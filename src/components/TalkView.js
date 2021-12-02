2aeb5f2ca36dc4213349195ad3b125295ff764eb
Kelvin Morrison <kelvinninja1@gmail.com>
2021-12-02 10:37:01 +0000

    Added: Conversation flow process through generate report

--- src/components/TalkView.js
+++ src/components/TalkView.js
@@ -1,51 +1,152 @@
 import React, { useState } from 'react'
-import { Skeleton, Typography, Button} from "@mui/material";
+import { Skeleton, Typography, Button, Dialog, Slide} from "@mui/material";
 import logo from '../logo.svg';
 import { Conversations } from "../data/Conversations";
-
-function getFirstName() {
-    let name = (localStorage.getItem("username") === null) ? "Guest": localStorage.getItem("username"); 
-    let firstName = (name.split(' ').length > 1) ? `${name.split(' ')[0]}`: `${name}`;
-    return firstName;
-  }
+import { Problems } from "../data/Problems";
+import ReportDialog from "../components/ReportDialog";
 
 function TalkView() {
+    const [flowStarted, setFlowStarted] = useState(false)
+    const [flowIndex, setFlowIndex] = useState(0)
     const [nextAsk, setNextAsk] = useState(0)
-    const [sayIntro, setSayIntro] = useState(true)
-    const [introduction, setIntroduction] = useState({
-        question: "Hello "+ getFirstName()+", I am your mental health companion. always here to help you thrive",
-        responses: [
-            { answer: "okay, Let's Talk", point: 1, problems: [] }
-        ]
-    })
-    const [ask, setAsk] = useState({
-        question: "How are you feeling?",
-        responses: [
-            { answer: "Happy", point: 1, problems: [] },
-            { answer: "Very Fine", point: 1, problems: [] },
-            { answer: "Okay", point: 1, problems: [] },
-            { answer: "Tired", point: 2, problems: [] },
-            { answer: "Stressed", point: 2, problems: [] },
-            { answer: "Sad", point: 2, problems: [] }
-        ]
-    })
-    // let convos = Conversations;
-    // let convo = {};
-    const conversationFlow = () => {
-        if (!(nextAsk + 1 > (Conversations.length) )){
-            setAsk(Conversations[nextAsk]);
-            console.log(Conversations.length);
-            if (!(nextAsk + 2 > (Conversations.length))){
+    const [ask, setAsk] = useState({})
+    const [reportIsReady, setReportIsReady] = useState(false)
+    const [activeProblems, setActiveProblems] = useState(Problems)
+    const [reportDialogOpen, setReportDialogOpen] = useState(false)
+    
+    
+    const conversationFlow = (points = 0, problems = [], key) => {
+        console.log(`Point: ${ points }`);
+        updatePoints(points, problems)
+
+        switch(flowIndex) {
+            case 0:
+              introAsksFlow();
+              break;
+            case 1:
+              feelingsAsksFlow();
+              break;
+            case 2:
+              leadAsksFlow();
+              break;
+            case 3:
+              followupAsksFlow();
+              break;
+            case 4:
+              closingAsksFlow();
+              break;
+            default:
+              feelingsAsksFlow();
+              break;
+          }
+    };
+
+    const introAsksFlow = () => {
+        if (!(nextAsk + 1 > (Conversations.introAsks.length) )){
+            setFlowStarted(true);
+            setAsk(Conversations.introAsks[nextAsk]);
+            console.log(Conversations.introAsks.length);
+            if (!(nextAsk + 2 > (Conversations.introAsks.length))){
                 setNextAsk(nextAsk + 1);
                 console.log(nextAsk);
+            } else {
+                setFlowIndex(flowIndex + 1);
+                setNextAsk(0);
+            }
+        }
+    }
+
+    const feelingsAsksFlow = () => {
+        if (!(nextAsk + 1 > (Conversations.feelingsAsks.length) )){
+            setAsk(Conversations.feelingsAsks[nextAsk]);
+            console.log(`Length: ${ Conversations.feelingsAsks.length }`);
+            if (!(nextAsk + 2 > (Conversations.feelingsAsks.length))){
+                setNextAsk(nextAsk + 1);
+                console.log(nextAsk);
+            } else {
+                setFlowIndex(flowIndex + 1);
+                setNextAsk(0);
+            }
+        }
+    }
+
+    const leadAsksFlow = () => {
+        if (!(nextAsk + 1 > (Conversations.leadAsks.length) )){
+            setAsk(Conversations.leadAsks[nextAsk]);
+            console.log(Conversations.leadAsks.length);
+            if (!(nextAsk + 2 > (Conversations.leadAsks.length))){
+                setNextAsk(nextAsk + 1);
+                console.log(nextAsk);
+            } else {
+                setFlowIndex(flowIndex + 1);
+                setNextAsk(0);
             }
         }
     };
 
-    const introFlow = () => {
-        setSayIntro(false); 
-        setIntroduction(introduction);
+    const followupAsksFlow = () => {
+        if (!(nextAsk + 1 > (Conversations.followupAsks.length) )){
+            setAsk(Conversations.followupAsks[nextAsk]);
+            console.log(Conversations.followupAsks.length);
+            if (!(nextAsk + 2 > (Conversations.followupAsks.length))){
+                setNextAsk(nextAsk + 1);
+                console.log(nextAsk);
+            } else {
+                setFlowIndex(flowIndex + 1);
+                setNextAsk(0);
+            }
+        }
+    };
+
+    const closingAsksFlow = () => {
+        if (!(nextAsk + 1 > (Conversations.closingAsks.length) )){
+            setAsk(Conversations.closingAsks[nextAsk]);
+            console.log(Conversations.closingAsks.length);
+            if (!(nextAsk + 2 > (Conversations.closingAsks.length))){
+                setNextAsk(nextAsk + 1);
+                console.log(nextAsk);
+            } else {
+                generateReport()
+            }
+        }
     }
+
+    const updatePoints = (points = 0, problems = []) => {
+        // TODO: Check If point is not zero or problems is not empty then
+        if(!(points === 0 || problems === [])) {
+            // TODO: Spread array into mutable object
+            let newProblems = [...activeProblems];
+            // TODO: Update points with problem as key
+            problems.map((problemKey, key) => {
+                newProblems[problemKey].points = newProblems[problemKey].points + points;
+                console.log(`Updated Points: ${newProblems[problemKey].points} `);
+                return true
+              });
+            // TODO: Set state
+            setActiveProblems(newProblems);
+        }
+        
+
+    }
+    const evaluateResponsesByPoints = () => {
+        // TODO: Sort Problems By points in descending order
+        console.log(activeProblems);
+        activeProblems.sort(function(a, b){return b.points-a.points});
+        console.log(activeProblems);
+    }
+
+    const generateReport = () => {
+        if(reportIsReady){
+            // TODO: Open Report Dialog
+            setReportDialogOpen(true);
+            console.log("Generating Report");
+        } else {
+            evaluateResponsesByPoints();
+            setReportIsReady(true);
+        }
+        
+    }
+
     const Logo = {
         title: "Thrive Logo",
         src: logo
@@ -68,40 +169,31 @@ function TalkView() {
             }
             
           {
-              sayIntro ? (
-                  <div>
-                    <Typography variant="h5" style={{textAlign: "left", padding: "10px"}} >{introduction.question}</Typography>
-  
-                    <div style={{textAlign: "right", padding: "10px"}}>
-                        { introduction.responses.map((response) => (
-                            <div>
-                                <div style={{flexGrow: 8}} />
-                                <Button onClick={() => introFlow()} color="primary" size="large" variant="outlined">{response.answer}</Button>
-                                <div style={{flexGrow: 2}} />
-                            </div>
-                        )) }
-                    </div>
-                  </div>
-                
-              ) : (
-                <div>                
-                    <Typography variant="h5">{ask.question}</Typography>
-    
-                    <div style={{textAlign: "right", paddingRight: "10px", margin: "10px"}}>
-                        { ask.responses.map((response) => (
-                            <div>
-                                <div style={{flexGrow: 8}} />
-                                <Button onClick={() => conversationFlow()} color="primary" size="large" variant="outlined" style={{mt: 1, mb: 1}} >{response.answer}</Button>
-                                <div style={{flexGrow: 2}} />
-                            </div>
-                        )) }
-                    </div>
+              (!flowStarted) ? conversationFlow() :
+            (<div>                
+                <Typography variant="h5">{ask.question}</Typography>
+
+                <div style={{textAlign: "right", paddingRight: "10px", margin: "10px"}}>
+                    { ask.responses.map((response) => (
+                        <div>
+                            <div style={{flexGrow: 9}} />
+                            <Button onClick={() => conversationFlow(response.point, ask.problems)} color="primary" size="large" variant="outlined" style={{mt: 1, mb: 1}} >{response.answer}</Button>
+                            <div style={{flexGrow: 1}} />
+                        </div>
+                    )) }
                 </div>
-              )
+            </div>)
           }
-            
+            <Dialog open={reportDialogOpen} fullScreen TransitionComponent={SlideTransition}>
+              <ReportDialog setSettingsDialogOpen={setReportDialogOpen} activeProblems={activeProblems}/>
+            </Dialog>
         </div>
     )
 }
+
+const SlideTransition = React.forwardRef(function TransitionComp(props, ref){
+    return <Slide direction="up" ref={ref} {...props} />
+})
+
 
 export default TalkView
--- src/data/Conversations.js
+++ src/data/Conversations.js
@@ -1,5 +1,41 @@
-export const Conversations = [
-        {question: "Are you experiencing fears or thoughts that are chronic (constant) and distressing and that interfere with daily living?",
+function getFirstName() {
+    let name = (localStorage.getItem("username") === null) ? "Guest": localStorage.getItem("username"); 
+    let firstName = (name.split(' ').length > 1) ? `${name.split(' ')[0]}`: `${name}`;
+    return firstName;
+  }
+  
+export const Conversations = {
+    introAsks: [
+        {
+            key: 0,
+            question: `Hello ${ getFirstName() }, I am your mental health companion. always here to help you thrive`,
+            responses: [
+                { answer: "okay, Let's Talk", point: 1, problems: [] }
+            ],
+            problems: []
+        }
+    ],
+
+    feelingsAsks: [
+        {
+            key: 1,
+            question: "How are you feeling?",
+            responses: [
+                { answer: "Happy", point: 1, problems: [] },
+                { answer: "Very Fine", point: 1, problems: [] },
+                { answer: "Okay", point: 1, problems: [] },
+                { answer: "Tired", point: 2, problems: [] },
+                { answer: "Stressed", point: 2, problems: [] },
+                { answer: "Sad", point: 2, problems: [] }
+            ],
+            problems: []
+        }
+    ],
+
+    leadAsks: [
+        {
+            key: 4,
+            question: "Are you experiencing fears or thoughts that are chronic (constant) and distressing and that interfere with daily living?",
             responses: [
                 { answer: "Yes", point: 2, problems: [] },
                 { answer: "No", point: 0, problems: [] },
@@ -7,7 +43,9 @@ export const Conversations = [
             ],
             problems: [0]
         },
-        {question: "I am going to take a guess and say you are not a child of 12 year old or less, right?",
+        {
+            key: 15,
+            question: "I am going to take a guess and say you are not a child of 12 year old or less, right?",
             responses: [
                 { answer: "Yes, I am not", point: 2, problems: [] },
                 { answer: "No, actually I am", point: 0, problems: [] },
@@ -15,7 +53,9 @@ export const Conversations = [
             ],
             problems: [1]
         },
-        {question: "Do you experience episodes of mood swings, including delusions and hallucinations?",
+        {
+            key: 6,
+            question: "Do you experience episodes of mood swings, including delusions and hallucinations?",
             responses: [
                 { answer: "Yes", point: 2, problems: [] },
                 { answer: "No", point: 0, problems: [] },
@@ -23,7 +63,9 @@ export const Conversations = [
             ],
             problems: [2]
         },
-        {question: "Have you stop enjoying your usual enjoyable activities?",
+        {
+            key: 7,
+            question: "Have you stop enjoying your usual enjoyable activities?",
             responses: [
                 { answer: "Yes", point: 2, problems: [] },
                 { answer: "No", point: 0, problems: [] },
@@ -31,7 +73,9 @@ export const Conversations = [
             ],
             problems: [3]
         },
-        {question: "Are you feeling disconnected from yourself?",
+        {
+            key: 8,
+            question: "Are you feeling disconnected from yourself?",
             responses: [
                 { answer: "Yes", point: 2, problems: [] },
                 { answer: "No", point: 0, problems: [] },
@@ -39,7 +83,9 @@ export const Conversations = [
             ],
             problems: [4]
         },
-        {question: "do you sometimes eat in secret? or engage in constant and excessive dieting?",
+        {
+            key: 9,
+            question: "Do you sometimes eat in secret? or engage in constant and excessive dieting?",
             responses: [
                 { answer: "Yes", point: 2, problems: [] },
                 { answer: "No", point: 0, problems: [] },
@@ -47,7 +93,9 @@ export const Conversations = [
             ],
             problems: [5]
         },
-        {question: "Are you fon of replacing a ‘bad thought’ with a ‘good thought’. ?",
+        {
+            key: 10,
+            question: "Are you fond of replacing a ‘bad thought’ with a ‘good thought’. ?",
             responses: [
                 { answer: "Yes", point: 2, problems: [] },
                 { answer: "No", point: 0, problems: [] },
@@ -55,7 +103,9 @@ export const Conversations = [
             ],
             problems: [6]
         },
-        {question: "Are you easily offended?",
+        {
+            key: 11,
+            question: "Are you easily offended?",
             responses: [
                 { answer: "Yes", point: 2, problems: [] },
                 { answer: "No", point: 0, problems: [] },
@@ -63,7 +113,9 @@ export const Conversations = [
             ],
             problems: [7]
         },
-        {question: "Are you avoiding reminders of the event, such as thoughts, feelings, people, places, activities or situations that bring back memories of the event.?",
+        {
+            key: 12,
+            question: "Are you avoiding reminders of the event, such as thoughts, feelings, people, places, activities or situations that bring back memories of the event.?",
             responses: [
                 { answer: "Yes", point: 2, problems: [] },
                 { answer: "No", point: 0, problems: [] },
@@ -71,7 +123,9 @@ export const Conversations = [
             ],
             problems: [8]
         },
-        {question: "Have you been hearing, seeing, smelling or tasting something that isn't there or experiencing any hallucinations?",
+        {
+            key: 13,
+            question: "Have you been hearing, seeing, smelling or tasting something that isn't there or experiencing any hallucinations?",
             responses: [
                 { answer: "Yes", point: 2, problems: [] },
                 { answer: "No", point: 0, problems: [] },
@@ -79,7 +133,9 @@ export const Conversations = [
             ],
             problems: [9, 10]
         },
-        {question: "You often lack of motivation?",
+        {
+            key: 14,
+            question: "You often lack of motivation?",
             responses: [
                 { answer: "Yes", point: 2, problems: [] },
                 { answer: "No", point: 0, problems: [] },
@@ -87,4 +143,47 @@ export const Conversations = [
             ],
             problems: [10]
         }
+    ],
+
+    followupAsks: [
+        {
+            key: 15,
+            question: "Once again, Are you fond of replacing a ‘bad thought’ with a ‘good thought’. ?",
+            responses: [
+                { answer: "Yes", point: 2, problems: [] },
+                { answer: "No", point: 0, problems: [] },
+                { answer: "I don't know", point: 1, problems: [] }
+            ],
+            problems: [6]
+        },
+        {
+            key: 16,
+            question: "Do you get easily offended?",
+            responses: [
+                { answer: "Yes", point: 2, problems: [] },
+                { answer: "No", point: 0, problems: [] },
+                { answer: "I don't know", point: 1, problems: [] }
+            ],
+            problems: [7]
+        }
+    ],
+
+    closingAsks: [
+        {
+            key: 2,
+            question: `Thank you, ${ getFirstName() }. All done! I have put together a report that outlines possible issues you might be facing`,
+            responses: [
+                { answer: "Okay, Show me", point: 0, problems: [] }
+            ],
+            problems: []
+        },
+        {
+            key: 3,
+            question: "Don't forget that this is not a medical diagnosis. If in doubt it is always best to seek advice from a medical professional",
+            responses: [
+                { answer: "Open Report", point: 0, problems: [] }
+            ],
+            problems: []
+        }
     ]
+}
