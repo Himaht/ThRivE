67997015be0e45a9d4976c1bd6b24adc2f828a91
Kelvin Morrison <kelvinninja1@gmail.com>
2021-12-02 11:45:11 +0000

    Added: Functionalities to the report dialog

--- src/components/ReportDialog.js
+++ src/components/ReportDialog.js
@@ -1,39 +1,7 @@
-import { Button, DialogActions, DialogTitle, AppBar, Toolbar, IconButton, Typography, FormLabel, Avatar } from "@material-ui/core";
+import { Button, DialogActions, DialogTitle, AppBar, Toolbar, IconButton, Typography, FormLabel, DialogContent, DialogContentText } from "@material-ui/core";
 import { ArrowBackIos } from "@material-ui/icons";
 
-function stringToColor(string) {
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = '#';
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.substr(-2);
-  }
-  /* eslint-enable no-bitwise */
-
-  return color;
-}
-
-
-function stringAvatar(name) {
-  name = (name === "") ? "Guest": name; 
-
-  let nameSplit = (name.split(' ').length > 1) ? `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`: `${name.split(' ')[0][0]}`;
-
-  return {
-    sx: {
-      bgcolor: stringToColor(name),
-    },
-    children: nameSplit,
-  };
-}
+import ProblemCard from "../components/ProblemCard";
 
 function getUsername() {
   let name = (localStorage.getItem("username") === null) ? "Guest": localStorage.getItem("username"); 
@@ -46,12 +14,6 @@ function ReportDialog({ setSettingsDialogOpen, activeProblems }) {
     setSettingsDialogOpen(false)
   };
 
-  const handleResetAccount = () => {
-    localStorage.clear();
-    console.log("Reset Account");
-    setSettingsDialogOpen(false)
-    window.location.reload(false);
-  };
   return (
     <div>
       <AppBar position="static" color="default">
@@ -59,23 +21,36 @@ function ReportDialog({ setSettingsDialogOpen, activeProblems }) {
                 <IconButton color="inherit" onClick={() => setSettingsDialogOpen(false)}>
                     <ArrowBackIos />
                     <Typography variant="h6" color="inherit" component="div">
-                        Your Report
+                    {getUsername()}'s Report
                     </Typography>
                 </IconButton>
                 <div style={{flexGrow: 1}} />
             </Toolbar>
         </AppBar>
-        <div>
-            <Toolbar>
-              <Avatar color="inherit" {...stringAvatar(getUsername())} />
-              <DialogTitle> {getUsername()} </DialogTitle>
-                <div style={{flexGrow: 1}} />
-            </Toolbar>
-        
-        </div>
+
+          <DialogTitle> <Typography variant="h4"> Feeling worried?  </Typography> </DialogTitle>
+          <DialogContent>
+          <DialogContentText>
+          <Typography variant="h5" color="inherit" component="span">
+          Don't hesitate to seek care, you should consult your doctor for further assessment and advice 
+          </Typography>
+          </DialogContentText>
+          <Typography variant="h5" color="inherit" component="span">
+          Possible Issues:
+          </Typography>
+          <div>                
+                {activeProblems.filter(problem => problem.points >= 1).map((problem, key) =>  {
+                  return (
+                    <div key={key}>
+                        <ProblemCard caption={problem.caption} brief={problem.brief} summary={problem.summary} symptoms={problem.symptoms} treatment={problem.treatment} help={problem.help}/>
+                    </div>
+                  );
+                })}
+            </div>
+        </DialogContent>        
+
         <DialogActions>
           <Button onClick={handleClose} color="primary" size="large" variant="outlined">Close</Button>
-          <Button onClick={handleResetAccount} style={{backgroundColor: '#CE1126', color: '#FFFFFF'}} size="large" variant="contained">Reset Account</Button>
         </DialogActions>
         <FormLabel><br/>...<br/></FormLabel>
     </div>
